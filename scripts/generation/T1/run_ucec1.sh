55d311194a64153198f23fdd240fb55d888b55f9
soroushsamadian <soroush.samadian@uhnresearch.ca>
2017-01-09 11:45:16 -0500

    scripts to generate files

---
+++ scripts/generation/T1/run_ucec1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#$ -cwd
+module load samtools/1.2
+module load bedtools
+module load sambamba/0.5.4
+module load vcftools
+
+python /mnt/work1/users/pughlab/projects/BAMgineer/src/simulate.py -vcf /mnt/work1/users/pughlab/projects/BAMgineer/inputs/vcfs/normal_varscan.vcf -cnv_amp /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/tcga/UCEC_1.gain.bed -cnv_del /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/tcga/UCEC_1.loss.bed  -inbam /mnt/work1/users/pughlab/projects/BAMgineer/inputs/bams/Normal.bam -outbam ucec1.bam -exons /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/exons.bed -r /mnt/work1/users/pughlab/projects/BAMgineer/ref/hg19.fasta -cancertype ucec -phase
---
+++ scripts/generation/T2/run_gbm2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#$ -cwd
+module load samtools/1.2
+module load bedtools
+module load sambamba/0.5.4
+module load vcftools
+
+python /mnt/work1/users/pughlab/projects/BAMgineer/src/simulate.py -vcf /mnt/work1/users/pughlab/projects/BAMgineer/inputs/vcfs/normal_varscan.vcf -cnv_amp /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/tcga/GBM_2.gain.bed -cnv_del /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/tcga/GBM_2.loss.bed  -inbam /mnt/work1/users/pughlab/projects/BAMgineer/inputs/bams/Normal.bam -outbam gbm2.bam -exons /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/exons.bed -r /mnt/work1/users/pughlab/projects/BAMgineer/ref/hg19.fasta -cancertype gbm -phase
+
