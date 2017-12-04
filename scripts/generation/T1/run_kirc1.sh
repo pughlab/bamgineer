5fb3e0083d10d9b79de87215e19540ab646d7b55
Soroush Samadian <soroush.samadian@uhnresearch.ca>
2017-12-04 15:27:33 -0500

    moved input and scripts to tcga dir

--- scripts/generation/T1/run_kirc1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#$ -cwd
-module load samtools/1.2
-module load bedtools
-module load sambamba/0.5.4
-module load vcftools
-python /mnt/work1/users/pughlab/projects/BAMgineer/src/simulate.py -vcf /mnt/work1/users/pughlab/projects/BAMgineer/inputs/vcfs/normal_varscan.vcf -cnv_amp /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/tcga/KIRC_1.gain.bed -cnv_del /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/tcga/KIRC_1.loss.bed  -inbam /mnt/work1/users/pughlab/projects/BAMgineer/inputs/bams/Normal.bam -outbam kirc1.bam -exons /mnt/work1/users/pughlab/projects/BAMgineer/inputs/beds/exons.bed -r /mnt/work1/users/pughlab/projects/BAMgineer/ref/hg19.fasta -cancertype kirc -phase
-
