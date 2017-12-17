f01726ecee64bda305c1859fb6e285872abf2a02
Soroush Samadian <soroush.samadian@uhnresearch.ca>
2017-12-17 11:34:54 -0500

    split the bam testing

--- src/utils.py
+++ src/utils.py
@@ -229,7 +229,6 @@ def subsample(bamfn1, bamfn2, samplingrate = 0.5):
 def splitBamByChr(inbamfn, path,chr):
 
     if(chr is not None ):
-        print("/".join([path,str(chr)+".bam"]))
         java_path, beagle_path, samtools_path, bedtools_path, vcftools_path, sambamba_path = params.GetSoftwarePath()
         command = " ".join([samtools_path, "view -bh", inbamfn, str(chr), ">",  "/".join([path,str(chr)+".bam"])])
         sortByName("/".join[path,str(chr)+".bam"], "/".join[path,str(chr)+".byname.bam"])
