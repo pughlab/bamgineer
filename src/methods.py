b40e319d9fe4dcf8dbef5286676a1c5090ca7ea7
Soroush Samadian <soroush.samadian@uhnresearch.ca>
2017-12-17 11:12:38 -0500

    split the bam testing

--- src/methods.py
+++ src/methods.py
@@ -224,10 +224,11 @@ def split_bam_by_chr(chr_list):
 
     inbam = params.GetInputBam()
     split_bam_path = params.GetSplitBamsPath()
+    print(split_bam_path)
     try:
         if not terminating.is_set():
             logger.debug("___ spliting bam by chromosome ___")
-            splitBamByChr( inbam ,splitbams,'chr'+str(chr_list))
+            splitBamByChr( inbam ,split_bam_path,'chr'+str(chr_list))
 
     except (KeyboardInterrupt):
         logger.error('Exception Crtl+C pressed in the child process  in split_bam_by_chr')
@@ -500,7 +501,6 @@ def run_pipeline(results_path):
         if(not params.GetSplitBamsPath()):
             chr_list = range(1, 22)
 
-            global splitbams
             splitbams = "/".join([res_path, 'splitbams'])
             params.SetSplitBamsPath(splitbams)
 
