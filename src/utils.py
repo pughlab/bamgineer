a3be8c6911033efef1d7d5d76f9cacddb05dbc54
Soroush Samadian <soroush.samadian@uhnresearch.ca>
2017-12-17 11:35:46 -0500

    split the bam testing

--- src/utils.py
+++ src/utils.py
@@ -231,7 +231,7 @@ def splitBamByChr(inbamfn, path,chr):
     if(chr is not None ):
         java_path, beagle_path, samtools_path, bedtools_path, vcftools_path, sambamba_path = params.GetSoftwarePath()
         command = " ".join([samtools_path, "view -bh", inbamfn, str(chr), ">",  "/".join([path,str(chr)+".bam"])])
-        sortByName("/".join[path,str(chr)+".bam"], "/".join[path,str(chr)+".byname.bam"])
+        sortByName("/".join([path,str(chr)+".bam"]), "/".join([path,str(chr)+".byname.bam"]))
         print(command)
         runCommand(command)
 
