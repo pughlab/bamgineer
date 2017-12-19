12eefbf91f17a08b47044c050474c597d3b1dace
Soroush Samadian <soroush.samadian@uhnresearch.ca>
2017-12-19 13:17:51 -0500

    debugging

--- src/qsub/ruffus/helpers/taskHelpers.py
+++ src/qsub/ruffus/helpers/taskHelpers.py
@@ -136,9 +136,9 @@ def CreateTaskList(inputs, sentinels, outputs, sample_ids, prev_sentinels):
        prev_sentinel_list = []
 
        for inpt in inputs:
-           input_list.append(inputs[count])
+           input_list.append(inpt[count])
            for output in outputs:
-               output_list.append(outputs[count])
+               output_list.append(output[count])
            for prev_sentinel in prev_sentinels:
                prev_sentinel_list.append(prev_sentinel[count])
 
