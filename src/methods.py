fd3cf3de643cf30a1b5b592fee9bb8c1d2b5380d
Soroush Samadian <soroush.samadian@uhnresearch.ca>
2017-12-27 10:42:53 -0500

    debugging

--- src/methods.py
+++ src/methods.py
@@ -94,7 +94,7 @@ def initialize(results_path,haplotype_path,cancer_dir_path):
     logger.debug("--- initialization complete ---")
     return
 
-def init_file_names(chr, event,tmpbams_path, haplotypedir):
+def init_file_names(chr, tmpbams_path, haplotypedir, event = None):
     
     flist=[]
 
